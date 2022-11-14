dd82c42991dc1a46160cb1acaafb77affc2413a3
wphong22 <118190935+wphong22@users.noreply.github.com>
2022-11-15 00:19:42 +0900

    commit

---
+++ frontend/src/components/DecreasestockCommand.vue
@@ -0,0 +1,62 @@
+<template>
+
+    <v-card outlined>
+        <v-card-title>
+            Decreasestock
+        </v-card-title>
+
+        <v-card-text>
+            <String label="Qty" v-model="value.qty" :editMode="editMode"/>
+        </v-card-text>
+
+        <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+                    color="deep-purple lighten-2"
+                    text
+                    @click="decreasestock"
+            >
+                Decreasestock
+            </v-btn>
+            
+            <v-btn
+                    color="deep-purple lighten-2"
+                    text
+                    @click="close"
+            >
+                Close
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+
+</template>
+
+<script>
+   
+    export default {
+        name: 'DecreasestockCommand',
+        components:{},
+        props: {},
+        data: () => ({
+            editMode: true,
+            value: {},
+        }),
+        created() {
+            this.value.qty = '';
+        },
+        watch: {
+        },
+        methods: {
+            decreasestock() {
+                this.$emit('decreasestock', this.value);
+            },
+            close() {
+                this.$emit('closeDialog');
+            },
+            change() {
+                this.$emit('input', this.value);
+            },
+        }
+    }
+</script>
+
---
+++ inventory/src/main/java/labshopmonolith/domain/DecreasestockCommand.java
@@ -0,0 +1,14 @@
+package labshopmonolith.domain;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.Date;
+import lombok.Data;
+
+@Data
+public class DecreasestockCommand {
+
+        private String qty;
+
+
+}
